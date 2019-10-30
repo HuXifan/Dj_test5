55bb30064ca3cdd40f6212bd53b7886be9ee3e2b
Huxifan <xifan_1995@163.com>
2019-10-30 15:20:27 +0800

    添加中间件

--- booktest/middleware.py
+++ booktest/middleware.py
@@ -13,3 +13,13 @@ class BlockIpMiddleware(object):
             return HttpResponse('<h1>Forbidden</h1>')
         else:
             return view_func(request, *view_args, **view_kwargs)
+
+
+'''
+在类中定义中间件预留函数。
+__init__:服务器响应第一个请求的时候调用。
+process_request:是在产生request对象，进行url匹配之前调用。
+process_view：是url匹配之后，调用视图函数之前。
+process_response：视图函数调用之后，内容返回给浏览器之前。
+process_exception:视图函数出现异常，会调用这个函数。
+'''
